b90fdc6eb942f68091e22c296052f2cf3c6e14e6
Awais Gohar <awaisgohar5432@gmail.com>
2020-01-07 10:47:53 +0500

    Updates

---
+++ precache-manifest.4bbf335fbb129602a72a40dba499cf72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae67d608cd2c9435f5b93956658d663",
+    "url": "/production2/index.html"
+  },
+  {
+    "revision": "27c60053621c714a8f0b",
+    "url": "/production2/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "bbbbdd650eebe52304e1",
+    "url": "/production2/static/css/main.0458d354.chunk.css"
+  },
+  {
+    "revision": "27c60053621c714a8f0b",
+    "url": "/production2/static/js/2.17c64f96.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/production2/static/js/2.17c64f96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bbbbdd650eebe52304e1",
+    "url": "/production2/static/js/main.909f43bb.chunk.js"
+  },
+  {
+    "revision": "f668240674588e9e60b1",
+    "url": "/production2/static/js/runtime-main.708de896.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/production2/static/media/logo.5d5d9eef.svg"
+  }
+]);
